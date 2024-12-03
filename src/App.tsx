aec75eedac07ccffc38801c8d044ed7fbffe6416
heygourab <gourabbg901@gmail.com>
2024-12-04 00:31:15 +0530

    feat: add react-router-dom for routing; update demo movie list structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { HomePage } from "./views/home/HomePage";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
