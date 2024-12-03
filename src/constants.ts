ac8927217f89ecf694416f191408988bf3bde1df
heygourab <gourabbg901@gmail.com>
2024-12-04 00:31:15 +0530

    feat: add react-router-dom for routing; update demo movie list structure

--- src/constants.ts
+++ src/constants.ts
@@ -13,20 +13,56 @@ export const placeHolderTexts = [
   "Search for a movie and let's make Hollywood jealous.",
 ];
 
-export const demoMoveList: MovieType = {
-  adult: false,
-  backdrop_path: "/inJjDhCjfhh3RtrJWBmmDqeuSYC.jpg",
-  genre_ids: [28, 878],
-  id: 399566,
-  original_language: "en",
-  original_title: "Godzilla vs. Kong",
-  overview:
-    "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
-  popularity: 10493.0,
-  poster_path: "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
-  release_date: "2021-03-24",
-  title: "Godzilla vs. Kong",
-  video: false,
-  vote_average: 8.4,
-  vote_count: 3612,
-};
+export const demoMoveList: MovieType[] = [
+  {
+    adult: false,
+    backdrop_path: "/inJjDhCjfhh3RtrJWBmmDqeuSYC.jpg",
+    genre_ids: [28, 878],
+    id: 399566,
+    original_language: "en",
+    original_title: "Godzilla vs. Kong",
+    overview:
+      "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+    popularity: 10493.0,
+    poster_path: "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
+    release_date: "2021-03-24",
+    title: "Godzilla vs. Kong",
+    video: false,
+    vote_average: 8.4,
+    vote_count: 3612,
+  },
+  {
+    adult: false,
+    backdrop_path: "/inJjDhCjfhh3RtrJWBmmDqeuSYC.jpg",
+    genre_ids: [28, 878],
+    id: 399566,
+    original_language: "en",
+    original_title: "Godzilla vs. Kong",
+    overview:
+      "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+    popularity: 10493.0,
+    poster_path: "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
+    release_date: "2021-03-24",
+    title: "Godzilla vs. Kong",
+    video: false,
+    vote_average: 8.4,
+    vote_count: 3612,
+  },
+  {
+    adult: false,
+    backdrop_path: "/inJjDhCjfhh3RtrJWBmmDqeuSYC.jpg",
+    genre_ids: [28, 878],
+    id: 399566,
+    original_language: "en",
+    original_title: "Godzilla vs. Kong",
+    overview:
+      "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+    popularity: 10493.0,
+    poster_path: "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
+    release_date: "2021-03-24",
+    title: "Godzilla vs. Kong",
+    video: false,
+    vote_average: 8.4,
+    vote_count: 3612,
+  },
+];
