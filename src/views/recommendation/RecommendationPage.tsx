d0836c563683d5d4a4175462bcdd852ea3e2c8df
heygourab <gourabbg901@gmail.com>
2024-12-10 10:20:32 +0530

    feat: simplify RecommendationPage component by removing unused imports and search functionality

--- src/views/recommendation/RecommendationPage.tsx
+++ src/views/recommendation/RecommendationPage.tsx
@@ -1,29 +1,13 @@
-import { MovieGenre } from "@/components/MovieGenre";
-
 import { AuroraBackground } from "@/components/ui/aurora-background";
-import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
-import { demoMoveList, placeHolderTexts } from "@/constants";
-import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-import { useState } from "react";
+import { demoMoveList } from "@/constants";
 import { MovieType } from "types";
 
+// Note: Recommendation Page
 export const RecommendationPage = ({
   movie = demoMoveList[0],
 }: {
   movie?: MovieType;
 }) => {
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Search Submitted:", searchQuery);
-    // Add further submit logic
-  };
-
   return (
     <AuroraBackground className="bg-slate-950 h-screen overflow-y-auto antialiased ">
       {undefined}
