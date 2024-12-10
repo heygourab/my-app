8a2d4566d0b15b205f0ef18d4c3ce4080dd900e3
heygourab <gourabbg901@gmail.com>
2024-12-10 10:18:11 +0530

    feat: remove redundant markup from RecommendationPage component

--- src/views/recommendation/RecommendationPage.tsx
+++ src/views/recommendation/RecommendationPage.tsx
@@ -25,50 +25,8 @@ export const RecommendationPage = ({
   };
 
   return (
-    <AuroraBackground className="bg-slate-950 h-screen overflow-y-auto antialiased">
-      <div className="relative flex flex-col items-start w-full min-h-full mx-auto">
-        {/* Search Bar Section */}
-        <header className="w-full sticky top-0 z-10 pt-4">
-          <PlaceholdersAndVanishInput
-            placeholders={placeHolderTexts}
-            onChange={handleChange}
-            onSubmit={handleSearchSubmit}
-          />
-        </header>
-
-        {/* Main Section */}
-        <main className="w-full ">
-          <AspectRatio ratio={16 / 9} className="relative w-full h-2/3 mt-4">
-            {/* Background Movie Poster */}
-            <img
-              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-              alt="Movie Poster"
-              className="w-full h-full rounded-3xl object-cover"
-              loading="lazy"
-            />
-            {/* Overlay Background */}
-            <div className="absolute inset-0 bg-gradient-to-t from-slate-950/70 to-transparent rounded-3xl" />
-            {/* Overlay Image */}
-            <div className="absolute left-4 transform -translate-y-1/2 w-3/5 h-96 gap-4  flex">
-              <img
-                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                alt="Overlay Poster"
-                className="rounded-3xl z-10 shadow-md"
-                loading="lazy"
-              />
-              <div className="flex flex-col">
-                <h1 className="text-6xl font-extrabold text-neutral-100">
-                  {movie.title}
-                </h1>
-                <p className="text-xl mt-4 text-neutral-400">
-                  {movie.overview}
-                </p>
-                <MovieGenre genresList={movie.genre_ids} />
-              </div>
-            </div>
-          </AspectRatio>
-        </main>
-      </div>
+    <AuroraBackground className="bg-slate-950 h-screen overflow-y-auto antialiased ">
+      {undefined}
     </AuroraBackground>
   );
 };
